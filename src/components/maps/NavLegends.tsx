d25e31959f7c6b1d09a3137f5754769fe71f5c6a
Brainlens <hfiz.ham@gmail.com>
2025-11-27 20:53:49 +0700

    update nav legend

--- src/components/maps/NavLegends.tsx
+++ src/components/maps/NavLegends.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 import MapsButton from "@/components/MapsButton";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 
 export default function NavLegends() {
 	return (
