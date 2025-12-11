4ad09816c196e79422621c847415847637a92ffd
Hafizh <hfiz.ham@gmail.com>
2025-12-11 21:51:00 +0800

    update rundown day 3

--- src/components/rundown/DayThree.tsx
+++ src/components/rundown/DayThree.tsx
@@ -4,7 +4,7 @@ import BacktoMain from "../BacktoMain";
 export default function DayThree() {
 	return (
 		<div>
-			<Image src="/rundown/day2.png" alt="Day 3" width={950} height={1600} />
+			<Image src="/rundown/day3.png" alt="Day 3" width={950} height={1600} />
 			<BacktoMain />
 		</div>
 	);
